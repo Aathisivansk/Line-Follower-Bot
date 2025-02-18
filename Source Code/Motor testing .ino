0bb520f79ef2ad7a7b535a7a163602a4b80458cb
Aathisivan1104 <aathisivan1104@gmail.com>
2025-02-18 19:31:58 +0530

    Changed code for Motor testong using L298N

--- Source Code/Motor testing .ino
+++ Source Code/Motor testing .ino
@@ -1,17 +1,23 @@
 // Motor driver control pins
-#define LEFT_MOTOR_FWD 5
-#define LEFT_MOTOR_BACK 6
-#define RIGHT_MOTOR_FWD 10
-#define RIGHT_MOTOR_BACK 11
+#define IN1 A1
+#define IN2 A2
+#define IN3 A3
+#define IN4 A4
+#define ENA 5
+#define ENB 6
 
 void setup()
 {
     //Setup for Motor Driver
-    pinMode(LEFT_MOTOR_FWD, OUTPUT);
-    pinMode(LEFT_MOTOR_BACK, OUTPUT);
-    pinMode(RIGHT_MOTOR_FWD, OUTPUT);
-    pinMode(RIGHT_MOTOR_BACK, OUTPUT);
-    
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+
+    Serial.begin(9600);
+
 }
 
 void loop()
@@ -22,33 +28,32 @@ void loop()
     backward();
     delay(2000);
 
-    stopMotor();
-    delay(2000);
-    
 }
 
 void forward()
 {
-    analogWrite(LEFT_MOTOR_FWD, 150);
-    analogWrite(LEFT_MOTOR_BACK, 0);
-    analogWrite(RIGHT_MOTOR_FWD, 150);
-    analogWrite(RIGHT_MOTOR_BACK, 0);
+    Serial.println("Moving Forward");
+    // Move Motor A forward at medium speed
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, 150);
+    // Move Motor B forward at medium speed
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, 150);
+
 
 }
 
 void backward()
 {
-    analogWrite(LEFT_MOTOR_FWD, 0);
-    analogWrite(LEFT_MOTOR_BACK,1);
-    analogWrite(RIGHT_MOTOR_FWD, 0);
-    analogWrite(RIGHT_MOTOR_BACK,1);
-}
-
-void stopMotor()
-{
-    //Turn OFF all motors
-    analogWrite(LEFT_MOTOR_FWD,0);
-    analogWrite(LEFT_MOTOR_BACK,0);
-    analogWrite(RIGHT_MOTOR_FWD,0);
-    analogWrite(RIGHT_MOTOR_BACK,0);
+    Serial.println("Moving Backward");
+    // Move Motor A forward at medium speed
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, 150);
+    // Move Motor B forward at medium speed
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENB, 150);
 }
